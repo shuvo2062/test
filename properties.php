de9ab845f5b4195853b2458161cd10dfcfc28ab3
shuvo2062@gmail.com <shuvo2062@gmail.com>
2022-01-03 16:44:33 +0000

    File generated by Skaffolder - user: shuvo2062@gmail.com

---
+++ properties.php
@@ -0,0 +1,10 @@
+<?php 
+	$db_test_db_Url = $_ENV["DB_HOST"] ?? '127.0.0.1';
+	$db_test_db_User = 'root';
+	$db_test_db_Pass = '';
+	$db_test_db_DbName = 'test_db';
+	
+	
+	$jwt_secret_key = 'mySecretKey';
+	
+?>
